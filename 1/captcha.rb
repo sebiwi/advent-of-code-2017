327db13101fc6555a9a068e7015eec52fb309e25
Sebastian Caceres <sebastian.caceresb@gmail.com>
2017-12-06 14:04:46 +0100

    Finish day 1 (sorry!)

--- 1/captcha.rb
+++ 1/captcha.rb
@@ -8,4 +8,15 @@ class Captcha
     result += array.last.to_i if array.last == array.first
     result
   end
+
+  def self.solve2 input
+    result = 0
+    elements = input.chars.map(&:to_i)
+    half_size =  elements.size/2
+    elements.each_with_index do |element, index|
+      result += element if element == elements[index + half_size]
+      break if index >= half_size
+    end
+    result * 2
+  end
 end
--- 1/spec/captcha_spec.rb
+++ 1/spec/captcha_spec.rb
@@ -24,6 +24,23 @@ describe Captcha do
       let(:input) {91212129}
       it { is_expected.to eq 9 }
     end
+  end
+  describe 'solve2' do
+    subject {Captcha.solve2 input}
 
+    context 'given 1212' do
+      let(:input) {'1212'}
+      it { is_expected.to eq 6 }
+    end
+
+    context 'given 1221' do
+      let(:input) {'1221'}
+      it { is_expected.to eq 0 }
+    end
+
+    context 'given 123425' do
+      let(:input) {'123425'}
+      it { is_expected.to eq 4 }
+    end
   end
 end
